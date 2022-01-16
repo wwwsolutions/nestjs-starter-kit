cfab5a6af6029dbbd5fcd7433d0897297b30ca97
Domagoj-Mario Mendas <42439126+wwwsolutions@users.noreply.github.com>
2022-01-16 21:37:56 +0100

    created AssertionUtils /w assertIsString + assertIsNumber methods

--- libs/shared/utils/src/index.ts
+++ libs/shared/utils/src/index.ts
@@ -1,6 +1,7 @@
 export * from './lib/authentication-utils.class';
 export * from './lib/conversion-utils.class';
 export * from './lib/debug-utils.class';
+export * from './lib/assert-utils.class';
 
 export * from './lib/filters/http-exception.filter';
 export * from './lib/filters/all-exception.filter';
---
+++ libs/shared/utils/src/lib/assertion-utils.class.ts
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { AssertionError } from 'assert';
+
+export class AssertionUtils {
+  static assertIsNumber(val: unknown): asserts val is number {
+    if (typeof val !== 'number') {
+      throw new AssertionError({ message: 'Not a number!' });
+    }
+  }
+
+  static assertIsString(val: unknown): asserts val is string {
+    if (typeof val !== 'string') {
+      throw new AssertionError({ message: 'Not a string!' });
+    }
+  }
+}
