a2d11011e95cfa32532e68926be29abc6e669f83
Domagoj-Mario Mendas <42439126+wwwsolutions@users.noreply.github.com>
2022-01-21 11:36:20 +0100

    generated api-integration-rest-mongoose lib

---
+++ libs/api/integration/rest-mongoose/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'api-integration-rest-mongoose',
+  preset: '../../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../../coverage/libs/api/integration/rest-mongoose',
+};
