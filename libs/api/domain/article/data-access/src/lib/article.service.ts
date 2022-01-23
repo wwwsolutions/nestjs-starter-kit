40bb321dee561cd1d77909c557b50a1aa3a0282a
Domagoj-Mario Mendas <42439126+wwwsolutions@users.noreply.github.com>
2022-01-23 23:59:41 +0100

    refactoring: moved article.model --> models/

--- libs/api/domain/article/data-access/src/lib/article.service.ts
+++ libs/api/domain/article/data-access/src/lib/article.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
-import { Article, ArticleDocument } from './article.model';
+import { Article, ArticleDocument } from './models/article.model';
 
 import { CreateArticleDto } from '@wwwsolutions/api/domain/article/dtos';
 
