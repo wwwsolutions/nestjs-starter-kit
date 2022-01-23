6df037f24b433a3d8e94ceaaffda9cca0bde934d
Domagoj-Mario Mendas <42439126+wwwsolutions@users.noreply.github.com>
2022-01-23 12:35:15 +0100

    generated api-domain-article-data-access lib

---
+++ libs/api/domain/article/data-access/jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  displayName: 'api-domain-article-data-access',
+  preset: '../../../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory:
+    '../../../../../coverage/libs/api/domain/article/data-access',
+};
