31ac5df95b992ea58fea89af4d401ca36018be4f
Domagoj-Mario Mendas <42439126+wwwsolutions@users.noreply.github.com>
2022-01-24 22:43:01 +0100

    implemented openapi generator on client side

--- apps/client-angular/src/app/app.component.ts
+++ apps/client-angular/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
+import { ArticlesApiService } from '@wwwsolutions/shared/data-access-api';
+
 @Component({
   selector: 'demo-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'client-angular';
+  articles$ = this.articles.articleControllerGetAllArticles();
+
+  constructor(private articles: ArticlesApiService) {}
 }
