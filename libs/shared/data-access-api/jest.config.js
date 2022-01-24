b818ed1b3d7d1c5a1c192d2d8908f87365ac32c9
Domagoj-Mario Mendas <42439126+wwwsolutions@users.noreply.github.com>
2022-01-24 21:05:34 +0100

    moved lib shared-api --> shared-data-access-api

--- libs/shared/data-access-api/jest.config.js
+++ libs/shared/data-access-api/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'shared-api',
+  displayName: 'shared-data-access-api',
   preset: '../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -8,7 +8,7 @@ module.exports = {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../../coverage/libs/shared/api',
+  coverageDirectory: '../../../coverage/libs/shared/data-access-api',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
