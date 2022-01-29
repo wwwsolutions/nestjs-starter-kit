274426c2782264154ab2e44431bb252108ac7499
Domagoj-Mario Mendas <42439126+wwwsolutions@users.noreply.github.com>
2022-01-29 22:21:30 +0100

    created AssertionUtils.assertIsObject method

--- libs/shared/utils/src/lib/assertion-utils.class.ts
+++ libs/shared/utils/src/lib/assertion-utils.class.ts
@@ -3,16 +3,33 @@ import { AssertionError } from 'assert';
 
 // see @hqoss/guards
 
+type ErrorMessage = { message: string };
+
 export class AssertionUtils {
-  static assertIsNumber(val: unknown): asserts val is number {
+  static assertIsNumber(
+    val: unknown,
+    errorMessage: ErrorMessage = { message: 'Not a number!' }
+  ): asserts val is number {
     if (typeof val !== 'number') {
-      throw new AssertionError({ message: 'Not a number!' });
+      throw new AssertionError(errorMessage);
     }
   }
 
-  static assertIsString(val: unknown): asserts val is string {
+  static assertIsString(
+    val: unknown,
+    errorMessage: ErrorMessage = { message: 'Not a string!' }
+  ): asserts val is string {
     if (typeof val !== 'string') {
-      throw new AssertionError({ message: 'Not a string!' });
+      throw new AssertionError(errorMessage);
+    }
+  }
+
+  static assertIsObject(
+    obj: unknown,
+    errorMessage: ErrorMessage = { message: 'Not a object!' }
+  ): asserts obj is object {
+    if (typeof obj !== 'object' || obj === null) {
+      throw new AssertionError(errorMessage);
     }
   }
 }
