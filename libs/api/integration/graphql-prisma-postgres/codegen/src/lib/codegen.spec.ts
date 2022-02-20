1f2425b1717278b5331e58da218b690b7425af17
Domagoj-Mario Mendas <42439126+wwwsolutions@users.noreply.github.com>
2022-02-20 21:13:38 +0100

    refactor: added code to api-integration-graphql-prisma-postgres-codegen

--- libs/api/integration/graphql-prisma-postgres/codegen/src/lib/codegen.spec.ts
+++
@@ -1,7 +0,0 @@
-import { codegen } from './codegen';
-
-describe('codegen', () => {
-  it('should work', () => {
-    expect(codegen()).toEqual('codegen');
-  });
-});
--- libs/api/integration/graphql-prisma-postgres/codegen/src/lib/codegen.ts
+++
@@ -1,3 +0,0 @@
-export function codegen(): string {
-  return 'codegen';
-}
